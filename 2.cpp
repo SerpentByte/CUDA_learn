7b86fc152cf0b017fb62a1e8f12041c273d9c393
Wasim <54527077+SerpentByte@users.noreply.github.com>
2020-02-05 19:13:56 +0530

    Add files via upload

--- 2.cpp
+++ 2.cpp
@@ -1,12 +1,9 @@
 #include<iostream>
-#include<cuda.h>
-#include<cuda_runtime.h>
 #include<ctime>
 #include<cstdlib>
 #include<cstdio>
 #include<unistd.h>
 #include<chrono>
-#include<omp.h>
 
 using namespace std;
 
@@ -17,12 +14,15 @@ int prng(int N=100)
 	return r;
 }
 
-__global__ void add_arrays(int *a, int *b, int *c, int n)
+int* scalar_add(int *a, int*b, int n)
 {
-	int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < n) {
-        c[i] = a[i] + b[i];
-    }
+	int* c = (int*)malloc(n*sizeof(int));
+	for(unsigned long long int i=0;i<n;i++)
+	{
+		c[i]=a[i]+b[i];
+	}
+
+	return c;
 }
 
 void display(int *A, int n)
@@ -54,42 +54,15 @@ int main(int argc, char* argv[])
 		c[i] = 0;
 	}
 
-	int *dev_a, *dev_b, *dev_c;
-
-	if(cudaMalloc((void**)&dev_a, sizeof(int)*n)==cudaSuccess
-		&& cudaMalloc((void**)&dev_b, sizeof(int)*n)==cudaSuccess
-		&& cudaMalloc((void**)&dev_c, sizeof(int)*n)==cudaSuccess)
-	{
-		NULL;
-	}
-	else
-	{
-		cout<<"Failed to allocate memory on device. Exiting.";
-		exit(0);
-	}
-
-	cudaMemcpy(dev_a, a, sizeof(int)*n,cudaMemcpyHostToDevice);
-	cudaMemcpy(dev_b, b, sizeof(int)*n,cudaMemcpyHostToDevice);
-	cudaMemcpy(dev_c, c, sizeof(int)*n,cudaMemcpyHostToDevice);
-
-	cout<<"Arrays copied to device."<<endl;
-
 	auto start = chrono::high_resolution_clock::now();
-	add_arrays<<<1+(n/256),n>>>(dev_a, dev_b, dev_c, n);
+	c = scalar_add(a,b,n);
 	auto end = chrono::high_resolution_clock::now();
 
 	double exec_t = chrono::duration_cast<chrono::microseconds>(end-start).count();
-	cudaMemcpy(c, dev_c, sizeof(int)*n,cudaMemcpyDeviceToHost);
-
-	cout<<"Sum copied from device."<<endl;
-
-	//display(c,n);
+	
 	cout<<"n = "<<n<<endl<<"Time taken = "<<exec_t<<" us\n";
 
 	free(a); free(b); free(c);
-	cudaFree(dev_a); cudaFree(dev_b); cudaFree(dev_c);
-
-
 
 	return 0;
 }
